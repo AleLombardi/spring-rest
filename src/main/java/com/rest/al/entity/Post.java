97d0c52465adcaf154d77d3b237403e181832101
AleLombardi <alessandro3lombardi@gmail.com>
2021-11-18 17:42:08 +0100

    new update

---
+++ src/main/java/com/rest/al/entity/Post.java
@@ -0,0 +1,69 @@
+package com.rest.al.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Post {
+
+	
+ private @Id  @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
+	
+	private String title;
+	
+	private String body;
+	
+	
+	public Post() {
+		
+	}
+
+
+	public Post(String title, String body) {
+		super();
+		this.title = title;
+		this.body = body;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getBody() {
+		return body;
+	}
+
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
